9bd0f5a69e2f4858a5b2654a4011c8f44f15bc7e
Khaled Romeh <64547704+Kairox00@users.noreply.github.com>
2023-06-01 14:09:55 +0300

    Updated seeds

--- src/main/java/com/indigo/bankservice/seeder/SeedRunner.java
+++ src/main/java/com/indigo/bankservice/seeder/SeedRunner.java
@@ -1,7 +1,6 @@
 package com.indigo.bankservice.seeder;
 
 import com.indigo.bankservice.models.BankAccount;
-import com.indigo.bankservice.models.Inventory;
 import com.indigo.bankservice.repositories.InventoryRepository;
 import com.indigo.bankservice.services.BankService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +12,15 @@ public class SeedRunner implements CommandLineRunner {
     @Autowired
     BankService bankService;
 
-     @Autowired
-     InventoryRepository inventoryService;
+    @Autowired
+    InventoryRepository inventoryService;
     @Override
     public void run(String... args) throws Exception {
         bankService.save(new BankAccount("123", 100));
         bankService.save(new BankAccount("456", 200));
-        inventoryService.save(new Inventory("1", 1));
-        inventoryService.save(new Inventory("2", 10));
-        System.out.println("Running methods after application startup...");
+//        inventoryService.save(new Inventory("1", 1));
+//        inventoryService.save(new Inventory("2", 10));
+        System.out.println("Seeding DB after application startup...");
     }
 
 }
